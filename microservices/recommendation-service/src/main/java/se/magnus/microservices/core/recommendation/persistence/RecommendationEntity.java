49e359af7f7899e3913cca3ac72aaf8ba22112c2
SunhyuckKim <sunhyuck.kim@gmail.com>
2021-06-04 09:38:53 +0900

    docker 적용, mongodb 적용 중 오류

---
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationEntity.java
@@ -0,0 +1,23 @@
+package se.magnus.microservices.core.recommendation.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "recommendations")
+@CompoundIndex(name = "prod-rec-id", unique = true, def = "{'productId': 1, 'recommendationId' : 1}")
+public class RecommendationEntity {
+
+    @Id
+    private String id;
+
+    @Version
+    private Integer version;
+
+    private int productId;
+    private int recommendationId;
+    private String author;
+    private String subject;
+    private String content;
+}
